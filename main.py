82af1f97d4086b5b986558a3339b34dba174d5ae
Farshad <asadpoor.f@gmail.com>
2022-06-17 22:42:21 +0430

    added the dockerignore and bugfix

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import imp
-from pickle import TRUE
 from redis_service import *
 from conf import REDIS_SERVER_IP
 
 redis = RedisService(host=REDIS_SERVER_IP)
-while TRUE:
+while True:
     try:
         redis.set_key()
     except Exception as e:
         print(f"Error:{e}")
     else:
-        print("key set")
+        print("key set successfully")
