d2eb4d2653cb19e187f169e5985b98e716158614
Farshad <asadpoor.f@gmail.com>
2022-06-17 22:52:13 +0430

    added the key counter

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from redis_service import *
 from conf import REDIS_SERVER_IP
 
 redis = RedisService(host=REDIS_SERVER_IP)
+counter = 1
 while True:
     try:
         redis.set_key()
     except Exception as e:
         print(f"Error:{e}")
     else:
-        print("key set successfully")
+        print(f"key set successfully key:{counter}")
+        counter += 1
